9ee34bddad9e94756e538d605420693e5617cc6b
Ronald Toussaint <r.toussaint@kopano.com>
2017-01-26 10:55:53 +0100

    Made sure user sees that he cannot disable the plugin when the admin has set PLUGIN_GOOGLE2FA_ALWAYS_ENABLED to true

--- php/plugin.google2fa.php
+++ php/plugin.google2fa.php
@@ -45,19 +45,20 @@ class PluginGoogle2FA extends Plugin {
 			case 'server.index.load.main.before' : // don't use the logon trigger because we need the settings
 
 				try {
+
 					if (PLUGIN_GOOGLE2FA_ALWAYS_ENABLED) {
 						$GLOBALS["settings"]->set('zarafa/v1/plugins/google2fa/enable', true);
 						$GLOBALS["settings"]->saveSettings();
 					}
-					
+
 					if (PLUGIN_GOOGLE2FA_ALWAYS_ACTIVATED)
 						Google2FAData::setActivate(true);
-				
+
 					// Check, if user has enabled plugin and has activated 2FA
 					if (!$GLOBALS["settings"]->get('zarafa/v1/plugins/google2fa/enable')
 						|| !Google2FAData::isActivated())
 						break;
-					
+
 					// Check, if Client-IP is in Whitelist
 					if (PLUGIN_GOOGLE2FA_WHITELIST !== "") {
 						foreach (explode (",", PLUGIN_GOOGLE2FA_WHITELIST) as $range) {
@@ -94,7 +95,7 @@ class PluginGoogle2FA extends Plugin {
 					$encryptionStore->add('password', '');
 
 					$_SESSION = array(); // clear session to logoff and don't loose session
-					
+
 					$_SESSION['google2FAUsername'] = $username; // or from $_POST/$GLOBALS
 					$_SESSION['google2FAPassword'] = $password;
 					$_SESSION['google2FASecret'] = Google2FAData::getSecret();
@@ -151,6 +152,7 @@ class PluginGoogle2FA extends Plugin {
 					'plugins' => Array(
 						'google2fa' => Array(
 							'enable' => PLUGIN_GOOGLE2FA_ENABLE,
+							'user_disable_allowed' => !PLUGIN_GOOGLE2FA_ALWAYS_ENABLED,
 							'enable_but_conf' => PLUGIN_GOOGLE2FA_ENBUTCONF,
 							'enable_but_activ' => PLUGIN_GOOGLE2FA_ENBUTACTIV,
 							'enable_but_tcodes' => PLUGIN_GOOGLE2FA_ENBUTTCODES,
